f05ba72077624036a9f7f099d4ab27f30945fdb1
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2023-12-06 19:21:50 +0500

    Masalalar yechildi

---
+++ index.js
@@ -0,0 +1,33 @@
+//  1 chi masala
+
+// let num1 = +prompt(`1 chi son`)
+// let num2 = +prompt(`2 chi son`)
+// let num3 = +prompt(`3 chi son`)
+// let result = num1 + num2 + num3
+// alert(result)
+
+
+//  2 chi masala
+
+// let number1 = +prompt(`1 chi son`)
+// let number2 = +prompt(`2 chi son`)
+// let num1Cvadrat = number1 * 2
+// let num2Cvadrat = number2 * 2
+// let result = num1Cvadrat + num2Cvadrat
+// alert(result)
+
+
+//  3 chi masala
+
+// let weight = +prompt(` to'gri tortburchakning uzunligi`)
+// let height = +prompt(`to'gri tortburchakning boyi`)
+// let result = (weight + height)*2
+// alert(result)
+
+
+//  4 chi masala
+
+
+
+
+
